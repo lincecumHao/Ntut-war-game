0b349ec1329e2f315f180296d0428b6c4dcb4e93
Lincecum <lincecum.hao@gmal.com>
2016-12-31 01:10:16 +0800

    Finish Login page structure.

--- client/components/auth/login.jsx
+++ client/components/auth/login.jsx
@@ -8,15 +8,23 @@ class Login extends Component {
                     <p className="title">Sign In</p>
                     <input placeholder="&#xf003;  Email" />
                     <input placeholder="&#xf084;  Password" />
-                    <a><p className="forgot-pwd">Forgot Password</p></a>
+                    <a><p className="default-font s14pt clickable">Forgot Password</p></a>
                     <div className="login-footer">
-                        <p>Remember Me</p>
-                        <button className="login-btn">Login</button>
+                        <input className="ckbx" type="checkbox" id="rember-me-ckbx" />
+                        <label htmlFor="rember-me-ckbx"><p className="default-font">Remember Me</p></label>
+                        <button className="default-font login-btn">Login</button>
                     </div>
                 </div>
                 <div className="login-padding">
-                    3RD LOGIN
-                        </div>
+                    <div className="align-left">
+                        <p className="default-font s21pt">Login with</p>
+                        <button className="auth-login-btn facebook clickable"></button>
+                        <button className="auth-login-btn google clickable"></button>
+                    </div>
+                    <div className="login-footer">
+                        <p className="default-font s21pt">Don't registered?</p>&#160;<p className="default-font s21pt clickable">Register now!</p>
+                    </div>
+                </div>
             </div>
         );
     }
