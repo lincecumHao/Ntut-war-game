1960fe7255535bef2b7ee07b68be60e3670cba94
Lincecum <lincecum.hao@gmal.com>
2017-01-24 10:47:10 +0800

    Add Google maps api

--- client/components/home/index.jsx
+++ client/components/home/index.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserContainer from './user/UserContainer.jsx';
 import Chatroom from './chatroom/Chatroom.jsx';
 import EagleMap from './map/EagleMap.jsx';
 import Status from './header/Status.jsx';
 import Resources from './resource/Resources.jsx';
 
-const Home = () => (
-    <div className="wrapper">
-        <div className="left">
-            <div className="header">
-                <UserContainer />
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.map = null;
+    }
+    
+
+    componentDidMount() {
+        this.map = new google.maps.Map(document.getElementById('eagle_map'), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="left">
+                    <div className="header">
+                        <UserContainer />
+                    </div>
+                    <Chatroom />
+                </div>
+                <div className="right">
+                    <div className="top">
+                        <EagleMap />
+                        <Status />
+                    </div>
+                    <div className="footer">
+                        <Resources />
+                    </div>
+                </div>
             </div>
-            <Chatroom />
-        </div>
-        <div className="right">
-            <div className="top">
-                <EagleMap />
-                <Status />
-            </div>
-            <div className="footer">
-                <Resources />
-            </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default Home;
--- client/components/home/map/EagleMap.jsx
+++ client/components/home/map/EagleMap.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class EagleMap extends Component {
     render() {
         return (
-            <div className="eagle_map">
+            <div className="eagle_map" id="eagle_map">
                 鷹眼圖
             </div>
         );
